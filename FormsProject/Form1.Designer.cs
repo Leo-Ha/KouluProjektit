6953b6b47f0b4bce4efc6dc014b1e589e2b80816
Leo-Ha <74101045+Leo-Ha@users.noreply.github.com>
2023-10-31 09:40:38 +0200

    Add files via upload

---
+++ FormsProject/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace FormsProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.ResultsPanel = new System.Windows.Forms.ListBox();
+            this.MakeTextBox = new System.Windows.Forms.TextBox();
+            this.ModelTextBox = new System.Windows.Forms.TextBox();
+            this.YearTextBox = new System.Windows.Forms.TextBox();
+            this.BodyTextBox = new System.Windows.Forms.TextBox();
+            this.FuelTextBox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.BackColor = System.Drawing.Color.Teal;
+            this.SearchBox.Font = new System.Drawing.Font("Arial", 20F);
+            this.SearchBox.ForeColor = System.Drawing.SystemColors.Menu;
+            this.SearchBox.Location = new System.Drawing.Point(14, 27);
+            this.SearchBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(293, 38);
+            this.SearchBox.TabIndex = 0;
+            this.SearchBox.Text = "Haku";
+            this.SearchBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ResultsPanel
+            // 
+            this.ResultsPanel.BackColor = System.Drawing.Color.Teal;
+            this.ResultsPanel.Font = new System.Drawing.Font("Arial", 15F);
+            this.ResultsPanel.ForeColor = System.Drawing.SystemColors.Menu;
+            this.ResultsPanel.FormattingEnabled = true;
+            this.ResultsPanel.ItemHeight = 23;
+            this.ResultsPanel.Location = new System.Drawing.Point(14, 82);
+            this.ResultsPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ResultsPanel.Name = "ResultsPanel";
+            this.ResultsPanel.Size = new System.Drawing.Size(289, 441);
+            this.ResultsPanel.TabIndex = 1;
+            this.ResultsPanel.SelectedIndexChanged += new System.EventHandler(this.ResultsPanel_SelectedIndexChanged);
+            // 
+            // MakeTextBox
+            // 
+            this.MakeTextBox.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.MakeTextBox.Font = new System.Drawing.Font("Arial", 20F);
+            this.MakeTextBox.ForeColor = System.Drawing.SystemColors.Menu;
+            this.MakeTextBox.Location = new System.Drawing.Point(394, 42);
+            this.MakeTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MakeTextBox.Name = "MakeTextBox";
+            this.MakeTextBox.Size = new System.Drawing.Size(209, 38);
+            this.MakeTextBox.TabIndex = 2;
+            this.MakeTextBox.Text = "Merkki";
+            this.MakeTextBox.TextChanged += new System.EventHandler(this.Make_TextChanged);
+            // 
+            // ModelTextBox
+            // 
+            this.ModelTextBox.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ModelTextBox.Font = new System.Drawing.Font("Arial", 20F);
+            this.ModelTextBox.ForeColor = System.Drawing.SystemColors.Menu;
+            this.ModelTextBox.Location = new System.Drawing.Point(394, 144);
+            this.ModelTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ModelTextBox.Name = "ModelTextBox";
+            this.ModelTextBox.Size = new System.Drawing.Size(209, 38);
+            this.ModelTextBox.TabIndex = 3;
+            this.ModelTextBox.Text = "Malli";
+            this.ModelTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // YearTextBox
+            // 
+            this.YearTextBox.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.YearTextBox.Font = new System.Drawing.Font("Arial", 20F);
+            this.YearTextBox.ForeColor = System.Drawing.SystemColors.Menu;
+            this.YearTextBox.Location = new System.Drawing.Point(394, 253);
+            this.YearTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.YearTextBox.Name = "YearTextBox";
+            this.YearTextBox.Size = new System.Drawing.Size(209, 38);
+            this.YearTextBox.TabIndex = 4;
+            this.YearTextBox.Text = "Vuosimalli";
+            this.YearTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // BodyTextBox
+            // 
+            this.BodyTextBox.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.BodyTextBox.Font = new System.Drawing.Font("Arial", 20F);
+            this.BodyTextBox.ForeColor = System.Drawing.SystemColors.Menu;
+            this.BodyTextBox.Location = new System.Drawing.Point(394, 363);
+            this.BodyTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.BodyTextBox.Name = "BodyTextBox";
+            this.BodyTextBox.Size = new System.Drawing.Size(209, 38);
+            this.BodyTextBox.TabIndex = 5;
+            this.BodyTextBox.Text = "Korimalli";
+            this.BodyTextBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // FuelTextBox
+            // 
+            this.FuelTextBox.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.FuelTextBox.Font = new System.Drawing.Font("Arial", 20F);
+            this.FuelTextBox.ForeColor = System.Drawing.SystemColors.Menu;
+            this.FuelTextBox.Location = new System.Drawing.Point(394, 479);
+            this.FuelTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.FuelTextBox.Name = "FuelTextBox";
+            this.FuelTextBox.Size = new System.Drawing.Size(209, 38);
+            this.FuelTextBox.TabIndex = 6;
+            this.FuelTextBox.Text = "Polttoaine";
+            this.FuelTextBox.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.SearchButton.Font = new System.Drawing.Font("Arial", 23F);
+            this.SearchButton.ForeColor = System.Drawing.Color.Snow;
+            this.SearchButton.Location = new System.Drawing.Point(725, 22);
+            this.SearchButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(153, 77);
+            this.SearchButton.TabIndex = 7;
+            this.SearchButton.Text = "Hae";
+            this.SearchButton.UseVisualStyleBackColor = false;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.UpdateButton.Font = new System.Drawing.Font("Arial", 23F);
+            this.UpdateButton.ForeColor = System.Drawing.Color.Snow;
+            this.UpdateButton.Location = new System.Drawing.Point(725, 233);
+            this.UpdateButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(153, 77);
+            this.UpdateButton.TabIndex = 8;
+            this.UpdateButton.Text = "Päivitä";
+            this.UpdateButton.UseVisualStyleBackColor = false;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.AddButton.Font = new System.Drawing.Font("Arial", 23F);
+            this.AddButton.ForeColor = System.Drawing.Color.Snow;
+            this.AddButton.Location = new System.Drawing.Point(725, 124);
+            this.AddButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(153, 77);
+            this.AddButton.TabIndex = 9;
+            this.AddButton.Text = "Lisää";
+            this.AddButton.UseVisualStyleBackColor = false;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClearButton.Font = new System.Drawing.Font("Arial", 23F);
+            this.ClearButton.ForeColor = System.Drawing.Color.Snow;
+            this.ClearButton.Location = new System.Drawing.Point(725, 343);
+            this.ClearButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(153, 77);
+            this.ClearButton.TabIndex = 10;
+            this.ClearButton.Text = "Tyhjennä";
+            this.ClearButton.UseVisualStyleBackColor = false;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.DeleteButton.Font = new System.Drawing.Font("Arial", 23F);
+            this.DeleteButton.ForeColor = System.Drawing.Color.Snow;
+            this.DeleteButton.Location = new System.Drawing.Point(725, 446);
+            this.DeleteButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(153, 77);
+            this.DeleteButton.TabIndex = 11;
+            this.DeleteButton.Text = "Poista";
+            this.DeleteButton.UseVisualStyleBackColor = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(933, 554);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.FuelTextBox);
+            this.Controls.Add(this.BodyTextBox);
+            this.Controls.Add(this.YearTextBox);
+            this.Controls.Add(this.ModelTextBox);
+            this.Controls.Add(this.MakeTextBox);
+            this.Controls.Add(this.ResultsPanel);
+            this.Controls.Add(this.SearchBox);
+            this.Font = new System.Drawing.Font("Arial", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SearchBox;
+        private System.Windows.Forms.ListBox ResultsPanel;
+        private System.Windows.Forms.TextBox MakeTextBox;
+        private System.Windows.Forms.TextBox ModelTextBox;
+        private System.Windows.Forms.TextBox YearTextBox;
+        private System.Windows.Forms.TextBox BodyTextBox;
+        private System.Windows.Forms.TextBox FuelTextBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button DeleteButton;
+    }
+}
+
---
+++ FormsProject/Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+
+namespace FormsProject
+{
+    public partial class MainForm : Form
+    {
+        private IFirebaseConfig config;
+        private FireSharp.FirebaseClient client;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeFirebase(); // Initialize Firebase configuration
+        }
+
+        public class CarModel
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public string Year { get; set; }
+            public string Body { get; set; }
+            public string Fuel { get; set; }
+        }
+
+        private void InitializeFirebase()
+        {
+            config = new FireSharp.Config.FirebaseConfig
+            {
+                AuthSecret = "D84WZonQIr3VqA91I1exNp2yWtJaJTjmR7DQEhR6",
+                BasePath = "https://fir-windows-d1432-default-rtdb.europe-west1.firebasedatabase.app/",
+            };
+
+            client = new FireSharp.FirebaseClient(config);
+            if (client != null)
+            {
+                // Firebase connection successful
+                MessageBox.Show("Firebase connection successful!");
+            }
+            else
+            {
+                // Handle Firebase connection error
+                MessageBox.Show("Failed to connect to Firebase!");
+            }
+        }
+
+
+        private Dictionary<string, CarModel> searchResults;
+
+
+
+
+
+        // Haku napin painallus
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            // Get the search query from the search box
+            string searchTerm = SearchBox.Text.ToLower(); // Convert the search term to lowercase
+
+            // Firebase path to your database node (replace 'cars' with your actual node name)
+            string firebasePath = "cars";
+
+            // Search for car models containing the search term
+            var response = client.Get(firebasePath);
+
+            // Clear the results panel and search results collection
+            ResultsPanel.Items.Clear();
+            searchResults = new Dictionary<string, CarModel>();
+
+            if (response.Body != "null")
+            {
+                var carModels = response.ResultAs<Dictionary<string, CarModel>>();
+
+                foreach (var carModel in carModels)
+                {
+                    if (carModel.Value.Make.ToLower().Contains(searchTerm)) // Convert to lowercase and use Contains
+                    {
+                        string resultText = $"{carModel.Value.Make} {carModel.Value.Model} ({carModel.Value.Year})";
+                        ResultsPanel.Items.Add(resultText);
+                        searchResults[resultText] = carModel.Value; // Store the CarModel object
+                    }
+                }
+
+                if (ResultsPanel.Items.Count == 0)
+                {
+                    MessageBox.Show("Virhe: ei tuloksia");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Haussa tapahtui virhe");
+            }
+        }
+
+
+
+
+
+        // Lisää napin painallus
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            // Validate input fields
+            if (!ValidateInput())
+            {
+                MessageBox.Show("Virheellinen syöte. Tarkista Vuosimalli kenttä.");
+                return;
+            }
+
+            // Create a new CarModel object with data from textboxes
+            var newCarModel = new CarModel
+            {
+                Make = MakeTextBox.Text,
+                Model = ModelTextBox.Text,
+                Year = YearTextBox.Text,
+                Body = BodyTextBox.Text,
+                Fuel = FuelTextBox.Text
+            };
+
+            // Generate a unique ID for the new record
+            string newCarModelId = GenerateUniqueID(); // Implement this method
+
+            // Firebase path to your database node (replace 'cars' with your actual node name)
+            string firebasePath = $"cars/{newCarModelId}";
+
+            // Add the new car model to the database
+            SetResponse response = client.Set(firebasePath, newCarModel);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                MessageBox.Show("Tietue lisätty");
+                ClearTextBoxes(); // Clear input textboxes after adding
+            }
+            else
+            {
+                MessageBox.Show("Tietuetta ei ovitu lisätä");
+            }
+        }
+
+        // Luo ID Lisäysnappia varten
+        private string GenerateUniqueID()
+        {
+            // Implement a method to generate a unique ID (e.g., using timestamp or random string)
+            // Return the generated ID
+            // Example using timestamp: return DateTime.Now.Ticks.ToString()
+            return Guid.NewGuid().ToString(); // Using a random GUID as an example
+        }
+
+
+
+
+
+        // ToDo:Inputtien validointi. Conformation tai error
+        private bool ValidateInput()
+        {
+            // Implement input validation, specifically for the "Year" field
+            // Return true if input is valid, otherwise return false
+            // Example: check if Year is a valid numeric value
+            if (!int.TryParse(YearTextBox.Text, out _))
+            {
+                return false;
+            }
+
+            return true;
+        }
+        
+
+
+
+
+        // Update napin painallus
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            if (ResultsPanel.SelectedIndex >= 0)
+            {
+                // Get the selected "Make" value from the ResultsPanel
+                string selectedMake = ResultsPanel.SelectedItem.ToString();
+
+                if (searchResults.ContainsKey(selectedMake))
+                {
+                    // Retrieve the CarModel object associated with the selected "Make"
+                    CarModel selectedCarModel = searchResults[selectedMake];
+
+                    // Get the unique ID of the selected car model
+                    string carModelId = GetCarModelId(selectedCarModel);
+
+                    // Create a car model object with updated information
+                    var updatedCarModel = new CarModel
+                    {
+                        Make = MakeTextBox.Text,
+                        Model = ModelTextBox.Text,
+                        Year = YearTextBox.Text,
+                        Body = BodyTextBox.Text, // Add the Body property
+                        Fuel = FuelTextBox.Text   // Add the Fuel property
+                    };
+
+                    // Firebase path to your database node (replace 'cars' with your actual node name)
+                    string firebasePath = $"cars/{carModelId}";
+
+                    // Update the car model in Firebase
+                    var response = client.Set(firebasePath, updatedCarModel);
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        MessageBox.Show("Car model updated successfully!");
+                        ClearTextBoxes(); // Clear input textboxes after updating
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to update car model.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select a car model to update.");
+                }
+            }
+        }
+
+        // Luo ID autoille jotta voit päivittää niitä
+        private string GetCarModelId(CarModel carModel)
+        {
+            string searchTerm = carModel.Make;
+
+            // Firebase path to your database node (replace 'cars' with your actual node name)
+            string firebasePath = "cars";
+            var response = client.Get(firebasePath);
+
+            if (response.Body != "null")
+            {
+                var carModels = response.ResultAs<Dictionary<string, CarModel>>();
+
+                foreach (var entry in carModels)
+                {
+                    if (entry.Value.Make.Equals(searchTerm, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return entry.Key; // Return the ID of the matching car model
+                    }
+                }
+            }
+
+            return string.Empty; // Return an empty string if no match is found
+        }
+
+
+
+
+
+        // Clear nappi painallus
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ClearTextBoxes(); // Call the ClearTextBoxes method to clear the textboxes
+        }
+
+        // Clear nappi toiminta
+        private void ClearTextBoxes()
+        {
+            MakeTextBox.Text = "";
+            ModelTextBox.Text = "";
+            YearTextBox.Text = "";
+            BodyTextBox.Text = "";
+            FuelTextBox.Text = "";
+            ResultsPanel.Items.Clear();
+        }
+
+
+
+
+
+/* TODO Delete function
+private void DeleteButton_Click(object sender, EventArgs e)
+{
+    if (ResultsPanel.SelectedIndex >= 0)
+    {
+        // Get the unique ID of the car model to delete
+        string carModelId = searchResults.Keys.ElementAt(ResultsPanel.SelectedIndex);
+
+        // Firebase path to your database node
+        string firebasePath = $"cars/{carModelId}";
+
+        // Delete the car model in Firebase
+        var response = client.Delete(firebasePath);
+
+        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            MessageBox.Show("Car deleted successfully");
+            ClearTextBoxes(); // Clear input textboxes after deleting
+        }
+        else
+        {
+            MessageBox.Show("Failed to delete car.");
+        }
+    }
+    else
+    {
+        MessageBox.Show("Please select a car to delete.");
+    }
+}
+*/
+
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+
+        private void ResultsPanel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ResultsPanel.SelectedIndex >= 0)
+            {
+                // Get the selected "Make" value from the ResultsPanel
+                string selectedMake = ResultsPanel.SelectedItem.ToString();
+
+                if (searchResults.ContainsKey(selectedMake))
+                {
+                    // Retrieve the CarModel object associated with the selected "Make"
+                    CarModel selectedCarModel = searchResults[selectedMake];
+
+                    // Display the information in the corresponding textboxes
+                    MakeTextBox.Text = selectedCarModel.Make;
+                    ModelTextBox.Text = selectedCarModel.Model;
+                    YearTextBox.Text = selectedCarModel.Year;
+                    BodyTextBox.Text = selectedCarModel.Body;
+                    FuelTextBox.Text = selectedCarModel.Fuel;
+                    // Add code to display Body and Fuel properties if needed
+                }
+            }
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+
+        private void Make_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+        }
+
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
